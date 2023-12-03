f8c6b04e6b087546bc2a61448b5cf5937ea59322
Drefdu <jesuseduardoarciniegamoreno@gmail.com>
2023-12-03 16:25:46 -0600

    cambios 3

--- database/config.js
+++ database/config.js
@@ -1,11 +1,12 @@
 const mongoose = require ('mongoose');
-MONGODB_CNN = 'mongodb+srv://daniela:danielaruiz@cluster1.8zhrttp.mongodb.net/?retryWrites=true&w=majority';
+MONGODB_CNN = 'mongodb+srv://dany:m8fd3UF2kntH1ANR@cluster0.mzxxqmm.mongodb.net/';
 const dbConnection = async () => {
     try {
         await mongoose.connect(MONGODB_CNN)
         console.log('Base de datos online :)')
     } catch (error) {
         console.log(error)  
+        mongoose.disconnect();
         throw new Error('Error al iniciar la base de datos')
     }
 };
