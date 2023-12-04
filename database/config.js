d4f0279b084a335e7be68d1add2f584fb424a1f6
Drefdu <jesuseduardoarciniegamoreno@gmail.com>
2023-12-03 20:29:29 -0600

    ahora si jala

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+        await mongoose.connect('mongodb+srv://daniela:VUTNb0om5m16vM8x@cluster0.otzdkns.mongodb.net/')
         console.log("Connect to MongoDB successfully")
     } catch (error) {
         console.log("Connect failed " + error.message )
